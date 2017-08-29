7fd2633cfb23fe50fd4a25a7f012cb22228aea54
kepennar <kevin.pennarun@gmail.com>
2017-08-29 16:59:07 +0200

    update dependencies

--- src/routes/credits/index.js
+++ src/routes/credits/index.js
@@ -1,11 +1,18 @@
 import { h, Component } from 'preact';
 import style from './style';
 
-export default () =>
+export default () => (
   <div class={style.credits}>
     <h1>Credits</h1>
     <ul>
-      <li>Icon from www.flaticon.com </li>
+      <li>
+        Icon from <a href="https://flaticon.com">www.flaticon.com</a>{' '}
+      </li>
       <li>An original idea from Nihad Amara</li>
+      <li>
+        This app is build with the help of the awesome
+        <a href="https://github.com/developit/preact-cli">Preact-cli</a>
+      </li>
     </ul>
-  </div>;
+  </div>
+);
